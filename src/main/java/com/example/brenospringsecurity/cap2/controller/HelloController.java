455341b73f0522d1b35c0721623ea83f391021f8
Breno <breno.csilva@hotmail.com>
2023-09-06 04:45:49 -0300

    first commit

---
+++ src/main/java/com/example/brenospringsecurity/cap2/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.brenospringsecurity.cap2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping
+    public String hello(){
+        return "Hello!";
+    }
+
+}
---
+++ src/test/java/com/example/brenospringsecurity/BrenoSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brenospringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrenoSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
